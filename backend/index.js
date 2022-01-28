f740cf43e4d13ee38b63a63d3159b5484e8400e7
mohammed-muddassir <muddassirmud15@gmail.com>
2022-01-28 23:10:12 +0530

    updated to fastify and used get operation

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const data = require('./scrapper');
+// const express = require('express');
+// const cors = require('cors');
+// const data = require('./scrapper');
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.get('/',async(req,res) => {
-    try{
-        const price= await data.getData()
-        return res.status(200).json({
-            result:price,
-        })
+// app.get('/',async(req,res) => {
+//     try{
+//         const price= await data.getData()
+//         return res.status(200).json({
+//             result:price,
+//         })
     
-    }
-    catch(err){
-        return res.status(500).json({
-            err:err.toString(),
-        })
-    }
-});
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             err:err.toString(),
+//         })
+//     }
+// });
 
-const PORT = 5000 || process.env;
+// const PORT = 5000 || process.env;
 
-app.listen(PORT,() => {
-    console.log(`Backend running on ${PORT}!`);
+// app.listen(PORT,() => {
+//     console.log(`Backend running on ${PORT}!`);
     
-});
+// });
