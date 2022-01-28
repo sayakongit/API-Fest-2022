a1e079a0c69fa446bd9e56a4acda6600fdf61972
mohammed-muddassir <muddassirmud15@gmail.com>
2022-01-28 09:09:51 +0530

    added gitignore and backend files

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const data = require('./scrapper');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',async(req,res) => {
+    try{
+        const price= await data.getData()
+        return res.status(200).json({
+            result:price,
+        })
+    
+    }
+    catch(err){
+        return res.status(500).json({
+            err:err.toString(),
+        })
+    }
+});
+
+const PORT = 5000 || process.env;
+
+app.listen(PORT,() => {
+    console.log(`Backend running on ${PORT}!`);
+    
+});
